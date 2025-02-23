f26137fb0d6225a21a5369c6d8cc237e4831011b
Naresh Bhatia <me@nareshbhatia.dev>
2025-02-23 03:48:12 -0500

    add tailwindcss and shadcn

--- src/app/routes/home/route.tsx
+++ src/app/routes/home/route.tsx
@@ -1,7 +1,7 @@
 export function HomePage() {
   return (
-    <div className="page">
-      <h1 className="heading1">Shadcn Starter</h1>
+    <div className="p-8">
+      <h1 className="text-xl font-medium">Shadcn Starter</h1>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   clearScreen: false,
   server: {
     port: 3000,
